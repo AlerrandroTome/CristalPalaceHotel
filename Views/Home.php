3d795b6346a9604fa3ef3aacf47f141b5e123f04
Alerrandro Tome <alerrandro.silva@ajaxti.net.br>
2020-08-01 14:10:51 -0300

    Configuração inicial da Home page.

---
+++ Views/Home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Css\HomeStyle.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>C.P | Home</title>
+</head>
+<body>
+    <div id="cont" class="container w-100 h-100">
+
+    </div>
+</body>
+</html>
